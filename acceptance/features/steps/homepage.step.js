8d7f6428bca7d4abce5d3be141515738d20a337c
Orhan Aygün <orh.aygun@gmail.com>
2022-01-29 00:00:50 +0300

    red phase for acceptance test 'User should see some videos on main page'

---
+++ acceptance/features/steps/homepage.step.js
@@ -0,0 +1,24 @@
+const {When, Then, Given} = require('cucumber');
+const puppeteer = require('puppeteer');
+const assert = require('assert');
+
+Given("that User goes to Video Site Project's HomePage", async () => {
+    this.browser = await puppeteer.launch();
+    this.page = await this.browser.newPage();
+})
+
+When("page is loaded", async () => {
+    await this.page.goto("http://localhost:8080")
+    await this.page.waitForTimeout(2000)
+})
+
+Then("User can see some of videos' title like", async (titles) => {
+    const titlesInPage = await this.page.$$eval('.video.title', el => el.textContent)
+    assert.ok(titlesInPage, "pages does not includes any video")
+    for (const [title] of titles.rawTable) {
+        assert.equal(titlesInPage.includes(title), true, `"${title}" not found`)
+    }
+
+    await this.page.close()
+    await this.browser.close()
+})
